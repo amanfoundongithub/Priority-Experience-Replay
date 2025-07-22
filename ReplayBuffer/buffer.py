06f0d3af4a4dd41bef27a6027737bad06ae2a90a
Aman Raj <billygates124356@gmail.com>
2025-07-22 11:11:53 +0530

    #5: Implemented add() and sample() in buffer

--- ReplayBuffer/buffer.py
+++ ReplayBuffer/buffer.py
@@ -11,6 +11,35 @@ class PriorityExperienceReplayBuffer:
         
         self.__alpha = alpha 
         self.__eps = min_priority
+        
     
+    def add(self, transistion : object, priority : float):
+        
+        priority = abs(priority)
+        priority = (priority + self.__eps) ** self.__alpha
+        
+        self.__tree.add(transistion, priority)
+    
+    def sample(self, batch_size : int = 64, beta : float = 0.6):
+        
+        total = self.__tree.total() 
+        segment = total / batch_size
+        
+        batch      = []
+        indexes    = []
+        priorities = []
+    
+        for i in range(batch_size):
+            
+            # Sampling value for random priority assignment
+            sampling_value = np.random.uniform(i * segment, (i + 1) * segment)
+            
+            # Get the item from the tree
+            idx, priority, data = self.__tree.find(sampling_value)
+            
+            # Append
+            batch.append(data)
+            indexes.append(idx)
+            priorities.append(priority)
         
         
